6ab1409ec8513cd96066643c1443cee978389f6e
lameck <lameck2@hotmail.com>
2018-03-21 11:22:28 +0000

    add files

--- pro_challenge/src/Port_scanner_42/main_42.java
+++ pro_challenge/src/Port_scanner_42/main_42.java
@@ -9,12 +9,11 @@ public class main_42 {
     public static void main(String[] args){
         System.out.println("PORT SCANNER");
         for (int port = 1; port <= 65535 ;port++){
-
             try {
                 Socket socket = new Socket();
                 socket.connect(new InetSocketAddress("localhost", port), 1000);
                 socket.close();
-                System.out.println("Porta: " + port + " está aberta!");
+                System.out.printf("Porta: " + port + " está aberta!\n");
 
             } catch(Exception ex){
 
